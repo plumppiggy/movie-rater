7a455e157833a329b86afbb534cac0f4d60f4879
Plumppiggy <elysiadarbourne@yahoo.ca>
2022-04-30 20:34:28 -0300

    updates

--- frontend/src/pages/movies.js
+++ frontend/src/pages/movies.js
@@ -11,14 +11,35 @@ class MoviesPage extends Component {
         creating: false
     };
 
+    constructor(props) {
+        super(props);
+        this.nameElRef = React.createRef();
+        this.ratingElRef = React.createRef();
+
+    }
+
     startCreateEventHandler = () => {
         this.setState({creating: true});
     };
     modalConfirmHandler = () => {
         this.setState({creating: false});
+        const name = this.nameElRef.current.value;
+        const rating = this.ratingElRef.current.value;
+
+        if (name.trim().length === 0 || rating.trim().length === 0) {
+            return;
+        }
+        
+        const event = {name, rating};
+
+        console.log(event);
+
+        
     };
     modalCancelHandler = () => {
         this.setState({creating: false});
+        
+
     };
 
     render () {
@@ -29,7 +50,14 @@ class MoviesPage extends Component {
             <Modal title="Add Movie" 
             canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
                 <form>
-                    
+                    <div className='form-control'>
+                        <label htmlFor='name'>Name</label>
+                        <input type="text" id='name' ref={this.nameElRef}></input>
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='rating'>Rating</label>
+                        <input type="number" id='rating' ref={this.ratingElRef}></input>
+                    </div>
                 </form>
             </Modal>}
             <div className='movies-control'>
